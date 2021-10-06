065a6e61d454dcec6ab1bf8c9d576b323aa34087
matthutt85 <matt.hutt85@gmail.com>
2021-10-06 16:41:26 +0100

    added api to beer cards, next to hook up filter to checkbox and beercard

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,24 +3,24 @@ import "./Nav.scss";
 
 
 
-const Nav = () => {
+const Nav = (props) => {
 
-
+const { inputValue, handleInput, handleClick } = props;
 
 
 
     return (
         <nav className="nav">
             <form className="nav__form">
-                <input type="text" className="nav__form-input" name="search"></input>
+                <input type="text" value={inputValue} onInput={handleInput} className="nav__form-input" name="search"></input>
             </form>
             <div className="checkbox">
                 <label for="checkbox1">High ABV (> 6.0%)</label>
-                <input type="checkbox" className="nav__form-checkbox1" name="checkbox1"></input>
+                <input type="checkbox" onclick={handleClick} className="nav__form-checkbox1" name="checkbox1"></input>
                 <label for="checkbox2">Classic Range</label>
-                <input type="checkbox" className="nav__form-checkbox1" name="checkbox2"></input>     
+                <input type="checkbox" onclick={handleClick} className="nav__form-checkbox1" name="checkbox2"></input>     
                 <label for="checkbox3">Acidic</label>
-                <input type="checkbox" className="nav__form-checkbox1" name="checkbox3"></input>               
+                <input type="checkbox" onclick={handleClick} className="nav__form-checkbox1" name="checkbox3"></input>               
             </div>
         </nav>
     )
